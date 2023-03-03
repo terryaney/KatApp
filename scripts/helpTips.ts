d30a30c53c32633b0d92b3b6b0ddeddb1a7a936f
Terry Aney <terry.aney@icloud.com>
2023-03-03 17:30:24 -0600

    Helptip from Modal, fixes #69124

--- scripts/helpTips.ts
+++ scripts/helpTips.ts
@@ -23,6 +23,7 @@
 	public static processHelpTips(application: KatApp, container: JQuery, selector?: string, tipsToProcess?: JQuery): void {
 
 		const isContainerKatApp = container.closest("[ka-id]").length == 1;
+		const isInsideModal = container.closest(".kaModal").length == 1;
 
 		// Wanted to enable helptip rendering/processing *outside* of a KatApp, so if container does not have a parent
 		// katapp, just let the selection be regular jquery.  Should probably restrict this from going INSIDE a katapp
@@ -132,7 +133,9 @@
 					html: true,
 					sanitize: false,
 					trigger: tipElement.data('bs-trigger') || "hover",
-					container: tipElement.data('bs-container') || "body",
+					// https://github.com/twbs/bootstrap/issues/22249#issuecomment-289069771
+					// There were some <a/> in popup from a kaModal that would not function properly until I changed the container.
+					container: tipElement.data('bs-container') || ( isInsideModal ? ".kaModal" : "body" ),
 					template: isTooltip
 						? '<div class="tooltip katapp-css" role="tooltip"><div class="tooltip-arrow arrow"></div><div class="tooltip-inner"></div></div>'
 						: '<div v-scope class="popover katapp-css" role="tooltip"><div class="popover-arrow arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
