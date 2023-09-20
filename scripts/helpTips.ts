0f4d5ee8e98c2c4751a94c2ed6a767af3cd00ff0
Terry Aney <terry.aney@icloud.com>
2023-09-20 11:06:01 -0500

    processDomElementsAsync handling button tooltips, fixes #1705221

--- scripts/helpTips.ts
+++ scripts/helpTips.ts
@@ -139,7 +139,7 @@
 				: content;
 		};
 
-		(tipsToProcess ?? select(selector ?? "[data-bs-toggle='tooltip'], [data-bs-toggle='popover']", container[0].tagName == "A" ? container.parent() : container ))
+		(tipsToProcess ?? select(selector ?? "[data-bs-toggle='tooltip'], [data-bs-toggle='popover']", container[0].tagName == "A" || container[0].tagName == "BUTTON" ? container.parent() : container ))
 			.not('[ka-init-tip="true"]')
 			.each((i, tip) => {
 				const tipElement = $(tip);
